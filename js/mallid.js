af10224a1d882097dacb50b226539a53db09e26f
FeldrinH <fhandswitch@gmail.com>
2020-11-07 00:02:14 +0200

    Parandatud kirjaviga. Puhastatud mallide koodi

--- js/mallid.js
+++ js/mallid.js
@@ -14,9 +14,9 @@ const mallid = {
     eksam: [
         {
             ülevaade: 'Pean minema hambaarsti juurde',
-            pealkiri: (_) => `Eksami aeg (${_.aine})`,
-            meil: (_) => meil(_,
-                `Ootamatult on selgunud, et mul on eksamigaa ga samal päeval hambaarstiaeg. Seetõttu palun võimalust teha eksam mõnel teisel ajal. Loodan et saate mulle selles osas vastu tulla.`
+            pealkiri: _ => `Eksami aeg (${_.aine})`,
+            meil: _ => meil(_,
+                `Ootamatult on selgunud, et mul on eksamiga samal päeval hambaarstiaeg. Seetõttu palun võimalust teha eksam mõnel teisel ajal. Loodan et saate mulle selles osas vastu tulla.`
             )
         }
     ],
@@ -29,8 +29,8 @@ const mallid = {
     kodutöö: [
         {
             ülevaade: 'Koer sõi naabri kassi ära',
-            pealkiri: (_) => `Kodutöö viivitus (${_.aine})`,
-            meil: (_) => meil(_,
+            pealkiri: _ => `Kodutöö viivitus (${_.aine})`,
+            meil: _ => meil(_,
                 `Olen sattunud mõnevõrra keerulisse olukorda, kuna eile õhtul sõi mu koer ootamatult naabri kassi ära. Seetõttu pean hetkel tegelema tekkinud segadusega ning ei saa kahjuks kodutööd õigeaegselt esitada.
                 Loodan, et mõistate olukorra tõsidust ning saate tähtaega pikendada.`
             )
