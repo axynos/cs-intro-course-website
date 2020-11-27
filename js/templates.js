63e9bb16760fae9491a42849bf396436d0c4af81
FeldrinH <fhandswitch@gmail.com>
2020-11-27 21:27:06 +0200

    Lisa mõned uued vabandused

--- js/templates.js
+++ js/templates.js
@@ -33,24 +33,45 @@ Loodan, et mõistate olukorra tõsidust ning saate tähtaega pikendada.`
             )
         },
         {
-            overview: 'Test 1',
-            heading: _ => `Kodutöö viivitus 1 (${_.subject})`,
+            overview: 'Koer sõi kodutöö ära',
+            heading: _ => `Tõsine probleem kodutööga (${_.subject})`,
             mail: _ => mail(_,
-`Pahandus ${_.subject} kodutöö ajal.`
+`Õnnetul kombel sõi mu koer eile õhtul ära nii minu kodutöö puhtandi kui ka kõik märkmed. Loodan, et saate anda kodutöö esitamiseks ajapikendust, kuna nagu isegi mõistate, ei ole mul võimalik hetkel midagi esitada.`
+            )
+        }, 
+        {
+            overview: 'Olen ajutiselt ilma internetita',
+            heading: _ => `Probleem kodutöö esitamisega (${_.subject})`,
+            mail: _ => mail(_,
+`Telia poolt teostatavate hooldustööde tõttu ei ole mul hetkel internetiühendust, ning seega ei saa ma kodutööd esitada. Esitan kodutöö niipea kui internetiühendus taastatakse. Loodan teie mõistvale suhtumisele.`
             )
         },
         {
-            overview: 'Test 2',
-            heading: _ => `Kodutöö viivitus 3 (${_.subject})`,
+            overview: 'Vajan aega kodutöös oleva materjali omandamiseks',
+            heading: _ => `Probleem kodutööga (${_.subject})`,
             mail: _ => mail(_,
-`Pahandus ${_.subject} kodutöö ajal.`
+`Olen oma õpingutega pisut maha jäänud ning vajan aega, et viia ennast kodutöös oleva materjaliga kurssi. Oma praeguste teadmiste juures ei ole ma võimeline kodutööd tegema ega esitma, kuid plaanin töötada oma teadmiste edendamise nimel ning loodan kodutöö lähemal ajal esitada. Loodan, et hindate minu pingutust ning olete tähtaja suhtes paindlik.`
+            )
+        }, 
+        {
+            overview: 'Pean minema hambaarsti juurde',
+            heading: _ => `Kodutöö aeg (${_.subject})`,
+            mail: _ => mail(_,
+`Ootamatult on selgunud, et mul on samal ajal kui plaanisin Teie aine kodutööd teha hambaarstiaeg. Seetõttu palun võimalust teha kodutöö mõnel teisel ajal. Loodan et saate mulle selles osas vastu tulla.`
             )
         },
         {
-            overview: 'Test 3',
-            heading: _ => `Kodutöö viivitus 3 (${_.subject})`,
+            overview: '',
+            heading: _ => ` (${_.subject})`,
             mail: _ => mail(_,
-`Pahandus ${_.subject} kodutöö ajal.`
+`TODO`
+            )
+        }, 
+        {
+            overview: '',
+            heading: _ => ` (${_.subject})`,
+            mail: _ => mail(_,
+`TODO`
             )
         }
     ]
