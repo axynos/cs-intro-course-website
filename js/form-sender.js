f5336d002751b2d0888375b02d6a60997d23a66d
FeldrinH <fhandswitch@gmail.com>
2020-11-30 20:14:07 +0200

    Muuda google formsi integratsiooni taaskasutatavamaks

--- js/form-sender.js
+++ js/form-sender.js
@@ -1,19 +1,19 @@
 // Hangime ankeedi sisendid
-const excuseForm = document.getElementById('excuse-form')
+const excuseForm = document.getElementById('submittable-form')
 
 // Funktsioon millega saadetakse ankeedi sisu google formsi
 // Loodud https://edcupaioli.com/blog/google-form-front-end/ põhjal
-const formsURL = 'https://docs.google.com/forms/d/e/1FAIpQLScpjOxsftNGBe3NcSJ2BJuTBhonHYr0WC_RVug0Ofu5bAL2gw/formResponse'
+const formsURL = ''
 function submitForm() {
     // entry.1834590686 - tõsidus
     // entry.1840013867 - vabandus
     // entry.2126048838 - meilinäidis
     const formURLParams = new URLSearchParams(new FormData(excuseForm))
-    fetch(`${formsURL}?${formURLParams.toString()}`, {mode: 'no-cors'})
+    fetch(`${excuseForm.dataset.target}?${formURLParams.toString()}`, {mode: 'no-cors'})
         .then(
             () => {
                 // Saatmine edukas (kui andmed on vigased siis ei pruugi vastus formsis kajastuda, isegi kui saatmine oli meie vaates edukas)
-                window.location.href = 'vabandus-saadetud'
+                window.location.href = excuseForm.dataset.action
             },
             () => {
                 // Saatmine kukkus läbi. See peaks juhtuma ainult siis kui internetiga on mingi jama
